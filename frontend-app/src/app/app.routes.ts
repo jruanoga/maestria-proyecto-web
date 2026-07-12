08e0d46f9bb6ebb5d2b31b0be1925f2b92d8e605
Joseph Ruano <jruanoga@gmail.com>
2026-07-12 00:23:26 -0600

    feat: Login y Dashboard

--- frontend-app/src/app/app.routes.ts
+++ frontend-app/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { LoginComponent } from './pages/login/login';
+import { Dashboard } from './pages/dashboard/dashboard';
+export const routes: Routes = [
+{ path: '', redirectTo: 'login', pathMatch: 'full' }, // Ruta por defecto
+{ path: 'login', component: LoginComponent },
+{ path: 'dashboard', component: Dashboard },
+];
--- frontend-app/src/app/pages/login/login.ts
+++ frontend-app/src/app/pages/login/login.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router'; // Para navegar mediante código
 @Component({
-  selector: 'app-login',
-  imports: [],
-  templateUrl: './login.html',
-  styleUrl: './login.css',
+selector: 'app-login',
+standalone: true,
+imports: [MatCardModule, MatInputModule, MatButtonModule], 
+templateUrl: './login.html', // (o './login.html')
+styleUrl: './login.css' // (o './login.css')
 })
-export class Login {}
+export class LoginComponent {
+// Inyectamos el router para poder cambiar de pantalla
+constructor(private router: Router) {}
+ingresarSistema() {
+// Por ahora, al hacer clic, simplemente vamos al dashboard
+this.router.navigate(['/dashboard']);
+}
+}
