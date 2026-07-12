21297154bb25d1e823beb2dc439eea44af93e828
Joseph Ruano <jruanoga@gmail.com>
2026-07-12 00:33:43 -0600

    Completado Sprint 1: UI de Login y Dashboard con Angular Material

--- frontend-app/src/app/app.routes.ts
+++ frontend-app/src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login';
-import { Dashboard } from './pages/dashboard/dashboard';
+import { DashboardComponent } from './pages/dashboard/dashboard';
 export const routes: Routes = [
 { path: '', redirectTo: 'login', pathMatch: 'full' }, // Ruta por defecto
 { path: 'login', component: LoginComponent },
-{ path: 'dashboard', component: Dashboard },
+{ path: 'dashboard', component: DashboardComponent },
 ];
--- frontend-app/src/app/pages/dashboard/dashboard.ts
+++ frontend-app/src/app/pages/dashboard/dashboard.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule
+  ],
   templateUrl: './dashboard.html',
-  styleUrl: './dashboard.css',
+  styleUrl: './dashboard.css'
 })
-export class Dashboard {}
+export class DashboardComponent {
+  nombreUsuario: string = 'Estudiante'; // luego vendrá del backend/login
+
+  constructor(private router: Router) {}
+
+  cerrarSesion(): void {
+    // Aquí luego limpiarás el token/sesión guardada
+    console.log('Cerrando sesión...');
+    this.router.navigate(['/login']);
+  }
+}
